135e9b049290221cdc5f459c2fd5f9c6ee264917
David <40313600+dlara319@users.noreply.github.com>
2019-01-30 19:34:55 -0800

    added code to login.js and created login.css

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import LoginWidget from "../components/LoginWidget";
-
-import "../styles/style.css";
+// import LoginWidget from "../components/LoginWidget";
+import FadeTransition from "../src/transitions/fadeTransition";
+import "../styles/login.css";
 
 class Login extends Component {
 
@@ -105,12 +105,195 @@ class RegisterBox extends React.Component{
         this.state = {
             username: "",
             email: "",
-            password: "";
-            errors:
+            password: "",
+            errors: {},
+            pwdState: null
+        };
+    }
+
+    showValidationErr(elm, msg) {
+        this.setState((prevState) => ({
+            errors: [
+                ...prevState.errors, {
+                    elm,
+                    msg
+                }
+            ]
+        }));
+    }
+
+    clearValidationErr(elm) {
+        this.setState((prevState) => {
+            let newArr = [];
+            for (let err of prevState.errors) {
+                if (elmj != err.elm) {
+                    newArr.push(err);
+                }
+            }
+            return {errors: newArr};
+        });
+    }
+
+    onUsernameChange(e) {
+        this.setState({username: e.state.value});
+        this.clearValidationErr("username");
+    }
+
+    onEmailChange(e) {
+        this.setState({email: e.target.value});
+        this.clearValidationErr("email");
+    }
+
+    onPasswordChange(e) {
+        this.setState({password: e.target.value});
+        this.clearValidationErr("password");
+
+        this.setState({pwdState: "too weak"});
+        if (e.target.value.lenght > 8) {
+            this.setState({pwdState: "medium"});
+        } else if (e.target.value.length > 12) {
+            this.setState({pwdState: "Very Strong"});
         }
     }
 
-}
+    openPopup(e) {
+        console.log("Hellooooo!")
+    }
 
-export default Login;
+    submitRegister(e) {
+
+        console.log(this.state);
+
+        if(this.state.username == "") {
+            this.showValidationErr("username", "Username is empty!");
+        }
+        if (this.state.email == "") {
+            this.showValidationErr("email", "Email is empty!");
+        }
+        if (this.state.password == "") {
+            this.showValidationErr("password", "Password is empty!");
+        }
+    }
+
+    render() {
+
+        let usernameErr = null,
+        passwordErr = null,
+        emailErr = null;
+
+        for (let err of this.state.errors) {
+            usernameErr = err.msg;
+        }
+        if (err.elm == "password") {
+            passwordErr = err.msg;
+        }
+        if (err.elm == "email") {
+            emailErr = err.msg;
+        }
+
+        let pwdWeak = false,
+        pwdMedium = false,
+        pwdStrong = false;
+  
+      if (this.state.pwdState == "weak") {
+        pwdWeak = true;
+      } else if (this.state.pwdState == "medium") {
+        pwdWeak = true;
+        pwdMedium = true;
+      } else if (this.state.pwdState == "strong") {
+        pwdWeak = true;
+        pwdMedium = true;
+        pwdStrong = true;
+      }
+
+      return (
+        <div className="inner-container">
+          <div className="header">
+            Register
+          </div>
+          <div className="box">
+  
+            <div className="input-group">
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                name="username"
+                className="login-input"
+                placeholder="Username"
+                onChange={this
+                .onUsernameChange
+                .bind(this)}/>
+              <small className="danger-error">{usernameErr
+                  ? usernameErr
+                  : ""}</small>
+            </div>
+  
+            <div className="input-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                name="email"
+                className="login-input"
+                placeholder="Email"
+                onChange={this
+                .onEmailChange
+                .bind(this)}/>
+              <small className="danger-error">{emailErr
+                  ? emailErr
+                  : ""}</small>
+            </div>
+  
+            <div className="input-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                className="login-input"
+                placeholder="Password"
+                onChange={this
+                .onPasswordChange
+                .bind(this)}/>
+              <small className="danger-error">{passwordErr
+                  ? passwordErr
+                  : ""}</small>
+  
+              {this.state.password && <div className="password-state">
+                <div
+                  className={"pwd pwd-weak " + (pwdWeak
+                  ? "show"
+                  : "")}></div>
+                <div
+                  className={"pwd pwd-medium " + (pwdMedium
+                  ? "show"
+                  : "")}></div>
+                <div
+                  className={"pwd pwd-strong " + (pwdStrong
+                  ? "show"
+                  : "")}></div>
+              </div>}
+  
+            </div>
+  
+            <button
+              type="button"
+              className="login-btn"
+              onHover={this
+              .openPopup
+              .bind(this)}
+              onClick={this
+              .openPopup
+              .bind(this)}>Register</button>
+  
+          </div>
+        </div>
+  
+      );
+  
+    }
+
+}
+reactDOM.render(
+    <App/>, document.getElementById("root"));
+  
+// export default Login;
 
