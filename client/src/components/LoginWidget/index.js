6944b80c83889e114878178fab74864fdb01886a
casekercan <casekercan@gmail.com>
2019-01-30 20:19:50 -0800

    git push origin dogList

--- client/src/components/LoginWidget/index.js
+++ client/src/components/LoginWidget/index.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
+//import FadeTransition from "../../transitions/fadeTransition";
 import "./style.css";
 
-function LoginWidget() {
-    return (
-        <h1> Login Widget with passport</h1>
-    );
+
+class LoginWidget extends Component {
+    render() {
+        return (
+            <div className="inner-container">
+                <div className="header">
+                    Login
+                </div>
+                <div className="box">
+                    <div className="input-group">
+                        <label htmlFor="username">Username</label>
+                        <input
+                            type="text"
+                            name="username"
+                            className="login-input"
+                            placeholder="Username" />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            className="login-input"
+                            placeholder="Password" />
+                    </div>
+                    <button
+                        type="button"
+                        className="login-btn">Login</button>
+                </div>
+            </div>
+        );
+    }
+
 }
 
 export default LoginWidget;
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,107 +1,16 @@
 import React, { Component } from "react";
-// import LoginWidget from "../components/LoginWidget";
-import FadeTransition from "../src/transitions/fadeTransition";
+import LoginWidget from "../components/LoginWidget";
 import "../styles/login.css";
 
 class Login extends Component {
-    render() {
-        return (
-            <div>
-                <h1> Login page </h1>
-                <LoginWidget />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <LoginWidget />
+      </div>
+    )
+  }
 }
 
-      return (
-        <div className="inner-container">
-          <div className="header">
-            Register
-          </div>
-          <div className="box">
-  
-            <div className="input-group">
-              <label htmlFor="username">Username</label>
-              <input
-                type="text"
-                name="username"
-                className="login-input"
-                placeholder="Username"
-                onChange={this
-                .onUsernameChange
-                .bind(this)}/>
-              <small className="danger-error">{usernameErr
-                  ? usernameErr
-                  : ""}</small>
-            </div>
-  
-            <div className="input-group">
-              <label htmlFor="email">Email</label>
-              <input
-                type="text"
-                name="email"
-                className="login-input"
-                placeholder="Email"
-                onChange={this
-                .onEmailChange
-                .bind(this)}/>
-              <small className="danger-error">{emailErr
-                  ? emailErr
-                  : ""}</small>
-            </div>
-  
-            <div className="input-group">
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                name="password"
-                className="login-input"
-                placeholder="Password"
-                onChange={this
-                .onPasswordChange
-                .bind(this)}/>
-              <small className="danger-error">{passwordErr
-                  ? passwordErr
-                  : ""}</small>
-  
-              {this.state.password && <div className="password-state">
-                <div
-                  className={"pwd pwd-weak " + (pwdWeak
-                  ? "show"
-                  : "")}></div>
-                <div
-                  className={"pwd pwd-medium " + (pwdMedium
-                  ? "show"
-                  : "")}></div>
-                <div
-                  className={"pwd pwd-strong " + (pwdStrong
-                  ? "show"
-                  : "")}></div>
-              </div>}
-  
-            </div>
-  
-            <button
-              type="button"
-              className="login-btn"
-              onHover={this
-              .openPopup
-              .bind(this)}
-              onClick={this
-              .openPopup
-              .bind(this)}>Register</button>
-  
-          </div>
-        </div>
-  
-      );
-  
-    }
-
-}
-reactDOM.render(
-    <App/>, document.getElementById("root"));
-  
-// export default Login;
+export default Login;
 
