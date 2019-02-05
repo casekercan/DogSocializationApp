3a282d3aec2a5ae8385d9c1c69445a5a10892501
Kamran Sahrakar <41097862+ksahrakar@users.noreply.github.com>
2019-02-04 16:55:36 -0800

    dog chckout modal added

--- client/src/components/DogListWidget/index.js
+++ client/src/components/DogListWidget/index.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import "./style.css";
 import API from "../../utils/API";
 import { Link } from "react-router-dom";
+import CheckoutDog from "./CheckoutDog";
 
 
 class DogListWidget extends Component {
     state = {
-        dogs: []
+        dogs: [],
+        show:false
     };
 
     componentDidMount() {
@@ -18,15 +20,27 @@ class DogListWidget extends Component {
         API.getDogs().then(res => this.setState({ dogs: res.data })).catch(err => console.log(err));
     };
 
+    showModal = () => {
+        this.setState({show:true})
+    }
+
+    hideModal = () => {
+        this.setState({show:false})
+    }
+
 
     render() {
         return (
             <table className="table table-striped">
+                <CheckoutDog show={this.state.show} handleClose={this.hideModal}>
+                    <p>Modal</p>
+                    <p>Data</p>
+                </CheckoutDog>
                 <thead>
                     <tr>
                         <th scope="col">Dog Name</th>
                         <th scope="col">Kennel #</th>
-                        <th scope="col">Socialization Progress</th>
+                        <th scope="col">Socialization Program</th>
                         <th scope="col"></th>
                     </tr>
                 </thead>
@@ -37,7 +51,7 @@ class DogListWidget extends Component {
                             <th>{dog.kennel}</th>
                             <th>
                                 {dog.socialization.map(soc => (
-                                    <span className="socEvent badge badge-primary text-wrap">{soc[0]}/{soc[1]}/{soc[2]}</span>
+                                    <span type="button" onClick={this.showModal} className="socEvent badge badge-primary text-wrap">{soc[0]}/{soc[1]}/{soc[2]}</span>
                                 ))}
                             </th>
                             <th>  <Link to={"/dog/" + dog._id} >More</Link> </th>
