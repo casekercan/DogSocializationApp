aa02847a25aecb56f43837e9a2fc81ec56b8fd4e
casekercan <casekercan@gmail.com>
2019-02-18 12:50:07 -0800

    updating express.static path

--- server.js
+++ server.js
@@ -35,7 +35,8 @@ app.use(routes);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
+	// app.use(express.static("client/build"));
+	app.use(express.static(path.resolve(__dirname, 'client/build')));
 
 }
 
