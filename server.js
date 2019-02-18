fddb6cc07a41598ed464aae960022299caf39ee6
casekercan <casekercan@gmail.com>
2019-02-18 12:54:57 -0800

    changed back

--- server.js
+++ server.js
@@ -35,8 +35,7 @@ app.use(routes);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-	// app.use(express.static("client/build"));
-	app.use(express.static(path.resolve(__dirname, 'client/build')));
+	app.use(express.static("client/build"));
 
 }
 
