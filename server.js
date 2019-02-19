b0f4406689bedc523a33b0156be6e9ff1e62f169
casekercan <casekercan@gmail.com>
2019-02-18 20:34:23 -0800

    added package-lock-json to gitignore

--- server.js
+++ server.js
@@ -38,7 +38,6 @@ app.use(routes);
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "client/build")));
-
 }
 
 // Connect to the Mongo DB
