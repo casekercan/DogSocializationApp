aff0aa3452b706fbbb193c76757559be488d0a49
casekercan <casekercan@gmail.com>
2019-02-18 22:11:07 -0800

    fixed server file

--- server.js
+++ server.js
@@ -40,12 +40,9 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Connect to the Mongo DB
+var CONNECTION_URI = process.env.MONGODB_URI || "mongodb://localhost/dogSocialization";
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/dogSocialization";
-
-mongoose.Promise = Promise;
-
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true }).then(() => {
+mongoose.connect(CONNECTION_URI, { useNewUrlParser: true }).then(() => {
 	console.log('Connected to MongoDB.');
 }).catch(err => console.log(err));
 
